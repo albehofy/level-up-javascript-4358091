3c53ce09e97097554827a4408fafe80b653b6adc
Mohamed Reda <121805783+albehofy@users.noreply.github.com>
2023-07-06 10:10:36 +0000

    01_02 chalange solution

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,16 @@
 // Write your code here
+class Movie {
+  constructor(title, director, genre, releaseYear, rating) {
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+  }
+
+  getOverView() {
+    return `${this.title} is a ${this.genre}, film directed by ${this.director}, was released in ${this.releaseYear}. it recived a rating of ${this.rating}`
+  }
+}
+const a = new Movie("newFilm","new Director","a new kind of movies","2023",7); 
+console.log(a.getOverView())
